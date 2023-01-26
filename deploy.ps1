88f404725bff5c5c33a829b4584013aa2b6b6979
WangTiantian139 <wangtiantian139@gmail.com>
2023-01-26 17:57:13 +0800

    fix the deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,15 @@
-npm run build
+# npm run build
 $server_root="../flask-server"
 if ( Test-Path $server_root/static -PathType Container ) {
     rm -r $server_root/static
-} else {
-    mkdir $server_root/static
 }
+mkdir $server_root/static
+
 if ( Test-Path $server_root/templates -PathType Container ) {
     rm -r $server_root/templates
-} else {
-    mkdir $server_root/templates
 }
+mkdir $server_root/templates
 
 cp dist/index.html $server_root/templates/
-cp -r dist/* $server_root/static/
+cp -r dist/* $server_root/static
 rm $server_root/static/index.html
